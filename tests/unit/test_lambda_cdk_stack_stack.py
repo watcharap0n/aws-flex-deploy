fb4f727b48b8b23f015ef302a01837122e6f283f
watcharapon weeraborirak <69833839+watcharap0n@users.noreply.github.com>
2025-06-05 14:22:56 +0700

    Fix unit test and update to use LambdaStack

--- tests/unit/test_lambda_cdk_stack_stack.py
+++ tests/unit/test_lambda_cdk_stack_stack.py
@@ -1,15 +1,27 @@
+"""Basic tests for the Lambda CDK stack."""
+
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from lambda_cdk_stack.lambda_cdk_stack_stack import LambdaCdkStackStack
+from cdk_stacks.lambda_stack import LambdaStack
 
-# example tests. To run these tests, uncomment this file along with the example
-# resource in lambda_cdk_stack/lambda_cdk_stack_stack.py
-def test_sqs_queue_created():
+
+def test_lambda_stack_synthesizes():
+    """Ensure the LambdaStack can be synthesized with minimal config."""
     app = core.App()
-    stack = LambdaCdkStackStack(app, "lambda-cdk-stack")
+    stack = LambdaStack(
+        app,
+        "test-stack",
+        project_config={
+            "lambda_function_name": "test_fn",
+            "code_from_asset": "_lambda",
+            "function_handler": "lambda_function.lambda_handler",
+        },
+    )
+
     template = assertions.Template.from_stack(stack)
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+    template.has_resource_properties(
+        "AWS::Lambda::Function",
+        {"Handler": "lambda_function.lambda_handler"},
+    )
